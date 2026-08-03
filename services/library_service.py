6fb0a25e51987a3a4b69a077b4c7e325b5300b3d
99Csui <99csui@gmail.com>
2026-08-02 20:37:34 -0400

    feat(services): complete LibraryService Sprint 2
    - Implement register_book()
    - Delegate book persistence to BookRepository
    - Return the registered Book instance
    - Preserve service responsibilities
    - Pass all LibraryService Sprint 2 tests

--- services/library_service.py
+++ services/library_service.py
@@ -2,9 +2,17 @@ from repositories.book_repository import BookRepository
 from repositories.member_repository import MemberRepository
 from repositories.loan_repository import LoanRepository
 
+from models.book import Book
+
+
 class LibraryService:
     def __init__(self, book_repository: BookRepository, member_repository: MemberRepository, loan_repository: LoanRepository):
         self._book_repository = book_repository
         self._member_repository = member_repository
         self._loan_repository = loan_repository
     
+    def register_book(self, book_id: int, title: str, author: str) -> Book:
+        book = Book(book_id, title, author)
+        self._book_repository.add(book)
+        return book
+
--- tests/services/test_library_service.py
+++ tests/services/test_library_service.py
@@ -1,8 +1,12 @@
 import unittest
+
 from repositories.book_repository import BookRepository
 from repositories.member_repository import MemberRepository
 from repositories.loan_repository import LoanRepository
 from services.library_service import LibraryService
+
+from models.book import Book
+
 
 class TestLibraryService(unittest.TestCase):
 
@@ -24,4 +28,29 @@ class TestLibraryService(unittest.TestCase):
     def test_library_service_keeps_loan_repository_dependency(self):
         self.assertIs(self.service._loan_repository, self.loan_repository)
 
+    def test_register_book_adds_book_to_repository(self):
+        stored_book = self.service.register_book(1, "Clean Code", "Robert C. Martin")
+        
+        self.assertEqual(self.book_repository.get_by_id(1), stored_book)
 
+    def test_register_book_returns_stored_book(self):
+        stored_book = self.service.register_book(1, "Clean Code", "Robert C. Martin")
+
+        self.assertIsInstance(stored_book, Book)
+        self.assertEqual(stored_book.id, 1)
+        self.assertEqual(stored_book.title, "Clean Code")
+        self.assertEqual(stored_book.author, "Robert C. Martin")
+
+    def test_register_book_stores_returned_book_instance(self):
+        stored_book = self.service.register_book(1, "Clean Code", "Robert C. Martin")
+        returned_book = self.book_repository.get_by_id(1)
+        
+        self.assertIs(stored_book, returned_book)
+
+    def test_register_book_raises_error_for_duplicate_book_id(self):
+        self.service.register_book(1, "Clean Code", "Robert C. Martin")
+        
+        with self.assertRaises(ValueError):
+            self.service.register_book(1, "Python Crash Course", "Eric Matthes")
+        
+        self.assertEqual(len(self.book_repository.list_all()), 1)
